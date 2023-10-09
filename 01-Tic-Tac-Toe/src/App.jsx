e980d1140cea3bf1b0921b082889f1500d8e9c00
Samir-Ahmimoud <sam.ahmimoud@gmail.com>
2023-10-09 06:47:45 +0100

    Tic Tac Toe
    Extract to componnents

--- 01-Tic-Tac-Toe/src/App.jsx
+++ 01-Tic-Tac-Toe/src/App.jsx
@@ -1,65 +1,18 @@
 import { useState } from 'react'
+import confetti from 'canvas-confetti'
+
+import { Square } from './components/Square.jsx'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { TURNS} from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import './App.css'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-
-const Square =({children, isSelected, updateBoard, index}) =>{
-  const className = `square ${isSelected ? 'is-selected': ''}`
-  
-  function handleClick(){
-    updateBoard(index)
-  }
-
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-
+import { Board } from './components/Board.jsx'
 
 function App() { 
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
   //null es que no hay ganador, false es que hay un empate
   const[winner, setWinner] = useState(null)
-
-  function checkWinner(boardToCheck) {
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo
-      if(
-        boardToCheck[a] && 
-        boardToCheck[a] === boardToCheck[b] && 
-        boardToCheck[a] === boardToCheck[c] 
-      ){
-        return boardToCheck[a]
-      }
-    }
-
-    return null
-  }
-
-  function checkEndGame(newBoard){
-    //revisamos si hay un empate
-    // si no hay mas espacios vacios
-    // en el tablero
-
-    return newBoard.every((square) => square != null)
-  }
 
   const updateBoard = (index) => {
     //no actualizamos esta posicion si ya tiene algo
@@ -73,9 +26,9 @@ function App() {
     setTurn(newTurn)
 
     //revisar si hay un ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
-      alert(`The winner is ${newWinner}`)
+      confetti()
       setWinner(newWinner)
     }else if(checkEndGame(newBoard)){
       setWinner(false) //Empate
@@ -92,50 +45,15 @@ function App() {
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className='game'>
-        {
-        board.map((_, index) =>{
-          return (
-          <Square 
-          key={index} 
-          index={index}
-          updateBoard={updateBoard}
-          >
-            {board[index]}
-          </Square>
-          )
-        })
-        }
-      </section>
+      
+      <Board board={board} updateBoard={updateBoard}/>
+
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}> {TURNS.X} </Square>
         <Square isSelected={turn === TURNS.O}> {TURNS.O} </Square>
       </section>
 
-      {
-        winner != null && (
-          <section className='winner'>
-            <div className='text'>
-              <h2>
-                {
-                  winner === false
-                  ?'Empate'
-                  : 'Gano'
-                }
-              </h2>
-
-              <header className='win'>
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
-              </footer>
-              
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
---
+++ 01-Tic-Tac-Toe/src/logic/board.js
@@ -0,0 +1,24 @@
+import { WINNER_COMBOS } from "../constants"
+
+export function checkWinnerFrom(boardToCheck) {
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c] 
+      ){
+        return boardToCheck[a]
+      }
+    }
+
+    return null
+  }
+
+export function checkEndGame(newBoard){
+  //revisamos si hay un empate
+  // si no hay mas espacios vacios
+  // en el tablero
+
+  return newBoard.every((square) => square != null)
+}
