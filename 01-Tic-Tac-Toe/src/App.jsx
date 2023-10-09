b584c06dc8e995e5dff68538959e139e0d0c8553
Samir-Ahmimoud <sam.ahmimoud@gmail.com>
2023-10-09 07:52:14 +0100

    Correct eslint standart

--- 01-Tic-Tac-Toe/src/App.jsx
+++ 01-Tic-Tac-Toe/src/App.jsx
@@ -3,14 +3,13 @@ import confetti from 'canvas-confetti'
 
 import { Square } from './components/Square.jsx'
 import { WinnerModal } from './components/WinnerModal.jsx'
-import { TURNS} from './constants.js'
+import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import './App.css'
 import { Board } from './components/Board.jsx'
 import { ResetGameToStorage, SaveGameToStorage } from './logic/index.js'
 
-function App() { 
-
+function App () {
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
     return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
@@ -20,35 +19,36 @@ function App() {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
   })
-  //null es que no hay ganador, false es que hay un empate
-  const[winner, setWinner] = useState(null)
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
-    //no actualizamos esta posicion si ya tiene algo
-    if(board[index] || winner) return
-    //actualaizar tablero
+    // no actualizamos esta posicion si ya tiene algo
+    if (board[index] || winner) return
+    // actualaizar tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    //cambiar turno
+    // cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     // guardar aqui partida
     SaveGameToStorage({
-      board: newBoard, 
-      turn: newTurn})
+      board: newBoard,
+      turn: newTurn
+    })
 
-    //revisar si hay un ganador
+    // revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
-    if(newWinner){
+    if (newWinner) {
       confetti()
       setWinner(newWinner)
-    }else if(checkEndGame(newBoard)){
-      setWinner(false) //Empate
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // Empate
     }
   }
 
-  function resetGame(){
+  function resetGame () {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
@@ -60,7 +60,7 @@ function App() {
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
 
-      <Board board={board} updateBoard={updateBoard}/>
+      <Board board={board} updateBoard={updateBoard} />
 
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}> {TURNS.X} </Square>
--- 01-Tic-Tac-Toe/src/logic/index.js
+++ 01-Tic-Tac-Toe/src/logic/index.js
@@ -1,9 +1,9 @@
-export function SaveGameToStorage({board, turn}){
-    window.localStorage.setItem('board', JSON.stringify(board))
-    window.localStorage.setItem('turn', turn)
+export function SaveGameToStorage ({ board, turn }) {
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', turn)
 }
 
-export function ResetGameToStorage(){
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
+export function ResetGameToStorage () {
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
 }
